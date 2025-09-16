0bfe03512a5d651cf66933bf3628e5a3db4ff862
Tjay05 <tjayolatayo@gmail.com>
2025-09-16 12:07:21 +0100

    Add logic for desktop view

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const menu = document.getElementById('menu');
 
 let displayValue = menu.style.display;
 
-hBurger.addEventListener('click', function() {
-  if (menu.style.display === displayValue) {
-    menu.style.display = 'flex';
-    hBurger.setAttribute('src', 'images/icon-close.svg');
-  } else {
-    menu.style.display = displayValue;
-    hBurger.setAttribute('src', 'images/icon-hamburger.svg');
+function mobileView () {
+  hBurger.addEventListener('click', function() {
+    if (menu.style.display === displayValue) {
+      menu.style.display = 'flex';
+      hBurger.setAttribute('src', 'images/icon-close.svg');
+    } else {
+      menu.style.display = displayValue;
+      hBurger.setAttribute('src', 'images/icon-hamburger.svg');
+    }
+  });
+}
+
+function desktopView() {
+	if (menu.style.display === 'flex') {
+		menu.style.display = 'none';
+	} else {
+    menu.style.display ='none';
   }
-})
+}
+
+function toggleView (e) {
+  if (e.matches) {
+    desktopView();
+  } else {
+    mobileView();
+  }
+}
+
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+toggleView(mediaQuery);
+mediaQuery.addEventListener('change', toggleView);
